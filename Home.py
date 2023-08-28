d3843b82613e47f5d1e706a7f1831e1229a2e6b0
kaiesalmahmud <kaies.rmedu03@gmail.com>
2023-08-28 10:25:54 +0600

    add homepage

---
+++ Home.py
@@ -0,0 +1,23 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="connectDB",
+    page_icon="🤖",
+)
+
+st.title("connectDB :star2:")
+st.write("### Your Natural Language Database Query Solution! 🤖")
+
+# st.sidebar.success("Select a Database.")
+
+st.markdown(
+    """
+Unlock the Power of Natural Language:
+Say goodbye to complex SQL queries and embrace the simplicity of natural language to interact with your databases effortlessly. DB Connect empowers you to communicate with your data just like talking to a friend.
+"""
+)
+st.markdown(
+    """
+    #### 👈 Select a Database from the sidebar 
+    """
+)
